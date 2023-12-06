ba63693be7fcfb2a63d7210ac77f014f53c9a351
aasmaanchaalak <84654827+aasmaanchaalak@users.noreply.github.com>
2023-12-06 23:55:42 +0530

    Update server.js

--- javascript/server.js
+++ javascript/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 // Middleware to parse JSON in request body
 app.use(express.json());
